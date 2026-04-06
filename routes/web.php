976e66cce014ce2d657b1ef8470b0fb9b8789b4e
vivian <viviansarahdiva25@gmail.com>
2026-04-06 20:59:05 +0700

    menambah route untuk dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengunjungController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard-pengunjung', [PengunjungController::class, 'dashboard']);
