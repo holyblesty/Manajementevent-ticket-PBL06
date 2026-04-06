e372496d10e7dc26b6db85ef6f23fd4561ae2ddf
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-04-06 23:45:01 +0700

    Menambahkan route dan fitur pada folder admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengunjungController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,6 @@ Route::get('/pembayaran', [PembayaranController::class, 'index']);
 Route::post('/pembayaran/proses', [PembayaranController::class, 'proses']);
 Route::get('/about', [PageController::class, 'about']);
 Route::get('/contact', [PageController::class, 'contact']);
+Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::get('/admin/events', [AdminController::class, 'events'])->name('admin.events');
+Route::get('/admin/participants', [AdminController::class, 'participants'])->name('admin.participants');
