7b1c3080ed71d0ad6343a5c455c31b274b4e791a
vivian <viviansarahdiva25@gmail.com>
2026-04-06 21:08:27 +0700

    menambah routes pembayaran

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengunjungController;
+use App\Http\Controllers\PembayaranController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 
 Route::get('/dashboard-pengunjung', [PengunjungController::class, 'dashboard']);
 Route::get('/pendaftaran', [PengunjungController::class, 'pendaftaran']);
+Route::get('/pembayaran', [PembayaranController::class, 'index']);
+Route::post('/pembayaran/proses', [PembayaranController::class, 'proses']);
