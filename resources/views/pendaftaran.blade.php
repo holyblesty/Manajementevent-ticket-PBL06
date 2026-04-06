1e3338820018d1b8b6fd938a1cdec36b820fe695
vivian <viviansarahdiva25@gmail.com>
2026-04-06 21:05:42 +0700

    menambah views pendaftaran

---
+++ resources/views/pendaftaran.blade.php
@@ -0,0 +1,11 @@
+<h1>Pendaftaran Event</h1>
+
+<form>
+<input type="text" placeholder="Nama">
+<select>
+<option>VIP</option>
+<option>Regular</option>
+</select>
+
+<button>Daftar</button>
+</form>
