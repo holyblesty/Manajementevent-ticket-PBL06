154f6a8bd71202fffcb2fed1019fd1551a9b2e42
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-04-06 23:02:52 +0700

    Menambahkan halaman About

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengunjungController;
 use App\Http\Controllers\PembayaranController;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,4 @@ Route::get('/dashboard-pengunjung', [PengunjungController::class, 'dashboard']);
 Route::get('/pendaftaran', [PengunjungController::class, 'pendaftaran']);
 Route::get('/pembayaran', [PembayaranController::class, 'index']);
 Route::post('/pembayaran/proses', [PembayaranController::class, 'proses']);
+Route::get('/about', [PageController::class, 'about']);
