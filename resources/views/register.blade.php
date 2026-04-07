4432cbc14594ce9583149eaf066f94acee9cf2e1
holyblesty <jesinaaurora@gmail.com>
2026-04-07 20:00:21 +0700

    menambahkan registerblade

---
+++ resources/views/register.blade.php
@@ -0,0 +1,8 @@
+<h1>Register</h1>
+
+<form>
+<input type="text" placeholder="nama">
+<input type="email" placeholder="email">
+<input type="password" placeholder="password">
+<button>Register</button>
+</form>
