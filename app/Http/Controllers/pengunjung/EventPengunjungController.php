8ce9082c7e67138c53469bb5c1fc3b14161efbd4
holyblesty <jesinaaurora@gmail.com>
2026-06-04 22:42:47 +0700

    ubah

--- app/Http/Controllers/pengunjung/EventPengunjungController.php
+++ app/Http/Controllers/pengunjung/EventPengunjungController.php
@@ -8,21 +8,27 @@ use Illuminate\Http\Request;
 
 class EventPengunjungController extends Controller
 {
-
     public function dashboard()
     {
-        $events = Event::with('tikets')->latest()->take(3)->get(); //mengambil seluruh data event
+        $events = Event::with('tikets')
+            ->latest()
+            ->take(3)
+            ->get();
+
         return view('beranda.index', compact('events'));
     }
 
     public function index()
     {
         $events = Event::with('tikets')->get();
+
         return view('events.index', compact('events'));
     }
+
     public function show($id)
     {
-        $event = Event::with('tikets')->findOrFail($id); //menampilkan detail event berdasarkan Id
+        $event = Event::with('tikets')->findOrFail($id);
+
         return view('events.show', compact('event'));
     }
 }
