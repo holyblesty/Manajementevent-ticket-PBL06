20c4aaf8c84fec01591fea11d48552d6ceb4aa13
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-06-18 06:43:24 +0700

    Update model Pemesanan terkait relasi atau atribut baru

--- app/Models/Pemesanan.php
+++ app/Models/Pemesanan.php
@@ -43,7 +43,7 @@ class pemesanan extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'id_pengunjung');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function event()
