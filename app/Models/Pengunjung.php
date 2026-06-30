a0ddebb8d1db3d6786b7bddb7e32b7f3659f2c95
lusianapnggbn-cell <lusipanggabean9@gmail.com>
2026-06-30 22:48:39 +0700

    memperbaiki atribut relasi yang ada pada database

--- app/Models/Pengunjung.php
+++ app/Models/Pengunjung.php
@@ -10,9 +10,8 @@ class Pengunjung extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-<<<<<<< HEAD
     // Menentukan nama tabel yang digunakan oleh model ini
-=======
+
     /*
     |--------------------------------------------------------------------------
     | CONFIG TABLE
@@ -20,7 +19,6 @@ class Pengunjung extends Authenticatable
     */
 
     // Nama tabel
->>>>>>> 0b98d7c3b4995202cf577c4b8a1d1121395af65b
     protected $table = 'pengunjung';
 
     // Primary Key
